357300d070a94600ac65f9540efb6c55d96cf2ce
giorgideveloper <gorgiladze888@gmail.com>
2025-08-07 18:09:04 +0400

    fix bug

--- src/app/components/MainPage/WigetEvents.js
+++ src/app/components/MainPage/WigetEvents.js
@@ -1,23 +1,57 @@
 
 import styles from './WigetEvents.module.css'; // Assuming you have a CSS module for WigetEvents styles
-
+import Image from 'next/image'; // Assuming you're using Next.js for image optimization
+import card from '../../image/card.webp'; // Adjust the path as necessary
 
 export default function WigetEvents() {
-  return (
-    <div className={`container-fluid px-0 ${styles.eventsSection}`}>
+return (
+    <div className={`container-fluid px-0 py-5 ${styles.eventsSection}`}>
         <div className="row text-center py-5">
             <h1>ღონისძიებების ადგილები</h1>
             <p>აქ შეგიძლიათ მოიძიოთ და მოაწყოთ ღონისძიებები</p>
         </div>
-        <div className="row">
+        <div className="row g-0">
             <div className="col-12 col-md-4">
-                
+                <div className={styles.imageContainer}>
+                    <Image
+                        src={card}
+                        alt="Event Card"
+                        style={{ width: '100%', height: 'auto' }}
+                    />
+                    <div className={styles.overlayContent}>
+                        <h3>სპორტული ვენიუები</h3>
+                      
+                    </div>
+                </div>
             </div>
-            <div className="col-12 col-md-4">2</div>
-            <div className="col-12 col-md-4">3</div>
+            <div className="col-12 col-md-4">
+                <div className={styles.imageContainer}>
+                    <Image
+                        src={card}
+                        alt="Event Card"
+                        style={{ width: '100%', height: 'auto' }}
+                    />
+                    <div className={styles.overlayContent}>
+                        <h3>სპორტული ვენიუები</h3>
 
+                    </div>
+                </div>
+            </div>
+            <div className="col-12 col-md-4">
+                <div className={styles.imageContainer}>
+                    <Image
+                        src={card}
+                        alt="Event Card"
+                        style={{ width: '100%', height: 'auto' }}
+                    />
+                    <div className={styles.overlayContent}>
+                        <h3>სპორტული ვენიუები</h3>
+
+                    </div>
+                </div>
+            </div>
+           
         </div>
-      
     </div>
-  )
+)
 }
