e5f58e76ac5ef589e55c6ef62f1e8f5a00c256a0
giorgideveloper <gorgiladze888@gmail.com>
2025-08-08 13:24:14 +0400

    add overlay in events

---
+++ src/app/components/MainPage/EventOverlay.js
@@ -0,0 +1,12 @@
+
+import styles from './WigetEvents.module.css'; 
+export default function EventOverlay() {
+  return (
+    <>
+        <div className={styles.eventHover}>
+         <h3 className='fs-1'>სპორტული ვენიუები</h3>
+             <button className="btn btn-light btn-lg ">სრულად</button>
+        </div>
+    </>
+  )
+}
