f41f929a9c6b48faed2cb3064a768e06f4b4e1fe
jorjodeveloper <gorgiladze888@gmail.com>
2025-11-14 15:38:23 +0400

    add main page image paralax

--- src/app/components/MainPage/MainPage.js
+++ src/app/components/MainPage/MainPage.js
@@ -1,35 +1,35 @@
 import { Slider } from "./Slider";
 import WhyBatumi from "./WhyBatumi";
 import { WigetEvent } from "./WigetEvent";
-import styles from './WhyBatumi.module.css'; 
+import styles from "./WhyBatumi.module.css";
 import { WigetBlog } from "./WigetBlog";
 import WigetEvents from "./WigetEvents";
 import VenueVideo from "./VenueVideo";
+import VenueImage from "./VenueImage";
 
- export  const MainPage = ({data, dict, lang }) => {
- 
-    return (
-        <>
-            <Slider data={data} dict={dict} lang={lang} />
-              <section  >
-                <WigetEvent data={data} dict={dict} lang={lang} />
-            </section>
-               <section>
-                <WigetBlog data={data} dict={dict} lang={lang} />
-            </section>
-          
-            <section className={styles.whyBatumiSection}>
-                <WhyBatumi data={data} />
-            </section>
-            <section>
-                <VenueVideo  data={data} />
-            </section>
-            <section>
-                <WigetEvents data={data} dict={dict} lang={lang} />
-            </section>
-            
-        </>
-    );
+export const MainPage = ({ data, dict, lang }) => {
+  return (
+    <>
+      <Slider data={data} dict={dict} lang={lang} />
+      <section>
+        <WigetEvent data={data} dict={dict} lang={lang} />
+      </section>
+      <section>
+        <WigetBlog data={data} dict={dict} lang={lang} />
+      </section>
+
+      <section className={styles.whyBatumiSection}>
+        <WhyBatumi data={data} />
+      </section>
+      <section>
+        {/* <VenueVideo data={data} /> */}
+        <VenueImage />
+      </section>
+      <section>
+        <WigetEvents data={data} dict={dict} lang={lang} />
+      </section>
+    </>
+  );
 };
 
 // {#EAECF0}
---
+++ src/app/components/MainPage/VenueImage.js
@@ -0,0 +1,58 @@
+"use client";
+import mainimage from "@/app/image/anbani2.png";
+import { useState } from "react";
+import styles from "./VenueImage.module.css";
+import { ImPlay2 } from "react-icons/im";
+import { IoCloseCircle } from "react-icons/io5";
+
+export default function VenueImage() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const handleClick = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  return (
+    <>
+      <div className={styles.venueImageContainer}>
+        <div className={styles.imageWrapper}>
+          <div className={styles.imageOverlay}>
+            <button onClick={handleClick} className="btn">
+              <ImPlay2 />
+            </button>
+          </div>
+          <div
+            className={styles.parallaxImage}
+            style={{
+              backgroundImage: `url(${mainimage.src})`,
+              backgroundAttachment: "fixed",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              width: "100%",
+              height: "600px",
+            }}
+          />
+        </div>
+      </div>
+
+      {isPlaying && (
+        <div className={styles.modal} onClick={handleClick}>
+          <div className={styles.modalContent}>
+            <button onClick={handleClick} className={styles.closeButton}>
+              <IoCloseCircle size={30} color="#fff" />
+            </button>
+            <iframe
+              width="800"
+              height="450"
+              src="https://www.youtube.com/embed/YG00FyDRFZI?autoplay=1"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
