030aceb1ac66767884b4924dc718a2971773d2f0
jorjodeveloper <gorgiladze888@gmail.com>
2025-11-14 15:53:38 +0400

    add lang in blog title

--- src/app/components/Blog/Blog.js
+++ src/app/components/Blog/Blog.js
@@ -1,43 +1,52 @@
-'use client';
-import React, { useState } from 'react';
-import LoadMore from '../LoadMore/LoadMore';
-import styles from './Blog.module.css'; 
-import BlogCard from './BlogCard';
-import ClientLink from './ClientLink';
+"use client";
+import React, { useState } from "react";
+import LoadMore from "../LoadMore/LoadMore";
+import styles from "./Blog.module.css";
+import BlogCard from "./BlogCard";
+import ClientLink from "./ClientLink";
 
-
-export default function Blog({data, lang, dict}) {
+export default function Blog({ data, lang, dict }) {
   const [post, setPost] = useState(data.results || []);
   const [page, setPage] = useState(2);
 
-
   return (
     <>
-    <div className="container-fluid px-0">
-      <div className="row text-center">
-        <div className={styles.header}> <h1>Media Blog</h1></div>
-      </div>
-    </div>
-    <div className="container">
-      <div className="row g-4">
-        {post?.map((item, index) => {
-          const href = `/${lang}/blog/${item.slug}`;
-          return (
-          <div className="col-12 col-lg-4" key={index}>
-            <ClientLink href={href}>
-              <BlogCard data={item} />
-            </ClientLink>
+      <div className="container-fluid px-0">
+        <div className="row text-center">
+          <div className={styles.header}>
+            {" "}
+            <h1 style={lang === "ka" ? { fontFamily: "firaGo" } : {}}>
+              {dict.blog.title}
+            </h1>
           </div>
-
-         );
-                })}
-
-           <div className="col-12">
-            <LoadMore dict={dict} lang={lang} items={data?.results} post={post} setPost={setPost} page={page} setPage={setPage} />
-           </div>
+        </div>
       </div>
-    </div>
+      <div className="container">
+        <div className="row g-4">
+          {post?.map((item, index) => {
+            const href = `/${lang}/blog/${item.slug}`;
+            return (
+              <div className="col-12 col-lg-4" key={index}>
+                <ClientLink href={href}>
+                  <BlogCard data={item} />
+                </ClientLink>
+              </div>
+            );
+          })}
+
+          <div className="col-12">
+            <LoadMore
+              dict={dict}
+              lang={lang}
+              items={data?.results}
+              post={post}
+              setPost={setPost}
+              page={page}
+              setPage={setPage}
+            />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
-    
--- src/app/components/LoadMore/LoadMore.js
+++ src/app/components/LoadMore/LoadMore.js
@@ -1,50 +1,54 @@
-'use client'
-import React, { useState } from 'react'
-import { fetchLoadMore } from '../../../service/service';
-import styles from './LoadMore.module.css';
+"use client";
+import React, { useState } from "react";
+import { fetchLoadMore } from "../../../service/service";
+import styles from "./LoadMore.module.css";
 
-export default function LoadMore({lang, items, post, setPost, page, setPage, mainUrl, dict}) {
-    const [haseMore, setHasMore] = useState(true);
-    const [loading, setLoading] = useState(false);
-    const [url, setUrl] = useState(mainUrl || '/pages/media-blog/?page=');
+export default function LoadMore({
+  lang,
+  items,
+  post,
+  setPost,
+  page,
+  setPage,
+  mainUrl,
+  dict,
+}) {
+  const [haseMore, setHasMore] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [url, setUrl] = useState(mainUrl || "/pages/media-blog/?page=");
 
-    
+  const handleLoadMore = async () => {
+    setLoading(true);
+    const data = await fetchLoadMore(lang, url, page);
 
-    
-
-    const handleLoadMore = async () => {
-       setLoading(true);
-       const data = await fetchLoadMore(lang, url, page)
-       
-       if (data?.results?.length > 0) {
-           setPost(prev => [...prev, ...data.results]);
-           setPage(prev => prev + 1);
-       }
-       else{
-           setHasMore(false);
-       }
-       setLoading(false);
-       setHasMore(false);
-      
+    if (data?.results?.length > 0) {
+      setPost((prev) => [...prev, ...data.results]);
+      setPage((prev) => prev + 1);
+    } else {
+      setHasMore(false);
     }
-    
+    setLoading(false);
+    setHasMore(false);
+  };
+
   return (
     <div className={`text-center ${styles.loadMoreButton}`}>
-    {haseMore &&(
-         <button className={`btn btn-outline-secondary rounded-5 fs-5`} style={{
-            border: '1px solid #000',
-            fontFamily: 'firaGo, sans-serif',
-            color: '#101828',
-            
-            
-         }} onClick={handleLoadMore} disabled={loading}>
-            <span>
-                 {loading ? 'Loading...' : dict['load-more'] || 'Load More'}
-            </span>
-     
-    </button>
-    )}
-   
+      {haseMore && (
+        <button
+          className={`btn btn-outline-secondary rounded-5 fs-5`}
+          style={{
+            border: "1px solid #000",
+            fontFamily: "firaGo, sans-serif",
+            color: "#101828",
+          }}
+          onClick={handleLoadMore}
+          disabled={loading}
+        >
+          <span>
+            {loading ? "Loading..." : dict["load-more"] || "Load More"}
+          </span>
+        </button>
+      )}
     </div>
-  )
+  );
 }
