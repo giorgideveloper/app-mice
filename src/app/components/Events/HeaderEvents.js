cfa43ce9f0e79dc4f82ea59c930c5f2b0ac0a3ec
jorjodeveloper <gorgiladze888@gmail.com>
2026-03-18 15:45:32 +0400

    add exebiton header image

--- src/app/components/Events/HeaderEvents.js
+++ src/app/components/Events/HeaderEvents.js
@@ -1,41 +1,48 @@
-import React, { useEffect } from 'react'
-import styles from './HeaderEvents.module.css'
-import eventsHeader from '@/app/image/exebitions.png'
-import ImageApp from '@/app/plugins/ImageApp'
-import event from '@/app/image/event-header2.png'
-import { fetchEventCategories } from '@/service/service'
+import React, { useEffect } from "react";
+import styles from "./HeaderEvents.module.css";
+import eventsHeader from "@/app/image/exebitions.png";
+import exebition from "@/app/image/exe1.jpeg";
 
+import ImageApp from "@/app/plugins/ImageApp";
+import event from "@/app/image/event-header2.png";
+import { fetchEventCategories } from "@/service/service";
 
-export default function HeaderEvents({title, lang}) {
+export default function HeaderEvents({ title, lang }) {
   const [eventCategory, setEventCategory] = React.useState([]);
-  
+
   useEffect(() => {
     const fetchCategorys = async () => {
       try {
         const data = await fetchEventCategories(lang);
         setEventCategory(data.results);
-
       } catch (error) {
-        console.error('Error fetching event categories:', error);
+        console.error("Error fetching event categories:", error);
       }
-    }
+    };
 
     fetchCategorys();
   }, []);
 
   return (
-    <div className='container-fluid px-0'>
+    <div className="container-fluid px-0">
       <div className="row">
         <div className="col-12">
           <div className={styles.headerImage}>
-          <ImageApp img={title.slug === "events" ? event : eventsHeader} alt="Events Header" />
-           <div className={styles.headerContent}>
-                  <h1 style={lang === "ka" ? { fontFamily: 'arialCaps' } : {}}>{title.slug === "events" ? eventCategory?.[0]?.name : eventCategory?.[1]?.name}</h1>
-                  {/* <p className="">Discover upcoming events, workshops, and activities to engage with our community and enhance your experience.</p> */}
-          </div>
+            <ImageApp
+              img={title.slug === "events" ? event : exebition}
+              alt="Events Header"
+            />
+            <div className={styles.headerContent}>
+              <h1 style={lang === "ka" ? { fontFamily: "arialCaps" } : {}}>
+                {title.slug === "events"
+                  ? eventCategory?.[0]?.name
+                  : eventCategory?.[1]?.name}
+              </h1>
+              {/* <p className="">Discover upcoming events, workshops, and activities to engage with our community and enhance your experience.</p> */}
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
